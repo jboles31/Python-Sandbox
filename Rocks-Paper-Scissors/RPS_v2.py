d5f3af4d6485fe4e7ca0e855c3fc699f1729d6fe
Jordan Boles <jordanboles1@gmail.com>
2020-01-29 14:56:17 -0700

    refactor of RPS

---
+++ Rocks-Paper-Scissors/RPS_v2.py
@@ -0,0 +1,30 @@
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+
+player1 = input("(enter Player 1's choice): ")
+print("*** NO CHEATING ***\n\n" * 20)
+player2 = input("(enter Player 2's choice): ")
+
+print("SHOOT!")
+
+if player1 == player2:
+  print('TIE')
+elif player1 == "rock":
+  if player2 == "scissors":
+    print('player1 wins!')
+  elif player2 == "paper":
+    print('player2 wins!')
+elif player1 == "paper":
+  if player2 == "rock":
+    print("player1 wins!")
+  elif player2 == "scissors":
+    print('player2 wins!')
+elif player1 == "scissors":
+  if player2 == "paper":
+    print("player1 wins!")
+  elif player2 == "rock":
+    print('player2 wins!')
+else:
+  print("something went wrong")
+
