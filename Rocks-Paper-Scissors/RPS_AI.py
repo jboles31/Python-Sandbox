1916321dc0f9e7345cfa6d0876b95c544796a497
Jordan Boles <jordanboles1@gmail.com>
2020-01-30 11:53:31 -0700

    added f string and .lower()

--- Rocks-Paper-Scissors/RPS_AI.py
+++ Rocks-Paper-Scissors/RPS_AI.py
@@ -4,16 +4,16 @@ print("...rock...")
 print("...paper...")
 print("...scissors...")
 
-player1 = input("(enter Player 1's choice): ")
+player1 = input("(enter Player's choice): ").lower()
 
 r = random.randint(1, 3)
 if r == 1:
   r = "rock"
 elif r == 2:
   r = "paper"
-elif r == 3:
+else:
   r = "scissors"
-print('Computer played ', r)
+print(f'Computer played {r}')
 
 print("SHOOT!")
 
@@ -21,18 +21,18 @@ if player1 == r:
   print('TIE')
 elif player1 == "rock":
   if r == "scissors":
-    print('player1 wins!')
-  elif r == "paper":
+    print('Player wins!')
+  else:
     print('The computer wins!')
 elif player1 == "paper":
   if r == "rock":
-    print("player1 wins!")
-  elif r == "scissors":
+    print("Player wins!")
+  else:
     print('The computer wins!')
 elif player1 == "scissors":
   if r == "paper":
-    print("player1 wins!")
-  elif r == "rock":
+    print("Player wins!")
+  else:
     print('The computer wins!')
 else:
   print("something went wrong")
