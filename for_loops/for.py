e33874de543669ebe7263e5d8726361813fe1110
Jordan Boles <jordanboles1@gmail.com>
2020-01-30 12:22:01 -0700

    for_loops

--- for_loops/for.py
+++ for_loops/for.py
@@ -2,5 +2,16 @@
 # 	print(x)
 # 	print(x*x)
 
-for letter in "coffee":
-	print(letter*10)
+# for letter in "coffee":
+# 	print(letter*2)
+
+# for char in "hockey":
+# 	print(char*3)
+
+# add all odd numbers up between 10,20
+x = 0
+
+for num in range(11,20,2):
+	x += num
+
+print(x)
