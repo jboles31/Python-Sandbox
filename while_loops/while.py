e1889f05fbfe783584166b6f5649c623c37023a9
Jordan Boles <jordanboles1@gmail.com>
2020-02-03 21:05:35 -0700

    finished exercise

--- while_loops/while.py
+++ while_loops/while.py
@@ -1,9 +1,9 @@
 # Continues to ask for user input until the user types 'bananas'
-msg = input("whats the secret password?")
-while msg != "bananas":
-	print("WRONG!")
-	msg = input("whats the secret password?")
-print("CORRECT!")
+# msg = input("whats the secret password?")
+# while msg != "bananas":
+# 	print("WRONG!")
+# 	msg = input("whats the secret password?")
+# print("CORRECT!")
 
 
 # for num in range(1,11):
