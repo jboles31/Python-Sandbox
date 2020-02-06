0bc573cb8d84266ef20f60af7a5517a8a1107994
Jordan Boles <jordanboles1@gmail.com>
2020-02-06 13:48:33 -0700

    used autopep8

--- guessing_games/guess_number.py
+++ guessing_games/guess_number.py
@@ -1,27 +1,20 @@
 from random import randint
 
-number = randint(1,10)
+number = randint(1, 10)
 msg = None
 
 while True:
-  msg = int(input("Guess a number between 1 and 10: "))
-  if msg < number:
-    print("Too low, try again")
-  elif msg > number:
-    print("Too high, try again!")
-  else:
-    print("You guessed it! You win!")
-    play_again = input("Do you want to play again? (y/n)")
-    if play_again == "y":
-      number = randint(1,10)
-      msg = None
+    msg = int(input("Guess a number between 1 and 10: "))
+    if msg < number:
+        print("Too low, try again")
+    elif msg > number:
+        print("Too high, try again!")
     else:
-      print("Thanks for playing")
-      break
-
-
-      
-    
-
-
-
+        print("You guessed it! You win!")
+        play_again = input("Do you want to play again? (y/n)")
+        if play_again == "y":
+            number = randint(1, 10)
+            msg = None
+        else:
+            print("Thanks for playing")
+            break
