404d71bc4a9c254c355781d6da622580e2e6c323
Jordan Boles <jordanboles1@gmail.com>
2020-02-07 18:02:43 -0700

    tuples

---
+++ tuples_sets/tuples.py
@@ -0,0 +1,14 @@
+# TUPLES
+# ordered collection of items
+# immutable
+# cannot update/remove from it
+
+tuple_example = (1,2,3)
+
+# why use tuple
+# faster and lighter weight than a list
+# tuples are valid keys in a dictionary, unlike a list
+
+# EXAMPLE of a good tuple
+# would be the months of the year
+# you don't need to ever change the months
