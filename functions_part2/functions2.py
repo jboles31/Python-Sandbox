e546d23f9457e9f3d64f41bbbb7b806f51fa34e6
Jordan Boles <jordanboles1@gmail.com>
2020-02-11 19:02:10 -0700

    keyword args, parameter ordering

--- functions_part2/functions2.py
+++ functions_part2/functions2.py
@@ -12,3 +12,36 @@ def all_sum(*args):
 
 print(all_sum(4,5,6))
 print(all_sum(4,5,6,7,8,9,10))
+
+
+# Keyword Arguments
+# 
+
+def fav_colors(**kwargs):
+  for person, color in kwargs.items():
+    print(f"{person}'s fav color is {color}'")
+
+fav_colors(colt="purple", ruby="red", ethel="teal")
+fav_colors(colt="purple", ruby="red", ethel="teal", ed="blue")
+fav_colors(colt="purple")
+
+# Exercise in Keyword Arguments
+
+def combine_words(word, **kwargs):
+  if "prefix" in kwargs:
+    return kwargs['prefix'] + word
+  elif "suffix" in kwargs:
+    return word + kwargs['suffix']
+  return word
+
+
+print(combine_words('child', prefix="man"))
+print(combine_words('child', suffix="ish"))
+
+
+# ORDERING OF PARAMETS
+
+# 1. - parameters
+# 2. - *args
+# 3. - default parameters
+# 4. - **kwargs
