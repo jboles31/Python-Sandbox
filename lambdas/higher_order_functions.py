891c1f8070b7d61fa7e702f8de3f47a25ce69120
Jordan Boles <jordanboles1@gmail.com>
2020-02-24 19:12:09 -0700

    filter/map

--- lambdas/higher_order_functions.py
+++ lambdas/higher_order_functions.py
@@ -22,3 +22,12 @@ l = [1,2,3,4]
 evens = list(filter(lambda x : x % 2 == 0, l))
 
 print(evens)
+
+names2 = ['jordan', 'evan', 'lucille', 'mack']
+
+# Map and Filter
+
+short_names = list(map(lambda name: f'Your friend is {name}',
+  filter(lambda value: len(value) < 5, names2)))
+
+print(short_names)
